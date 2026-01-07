39d93fdcaf8c2bf3ab7d2bbef3acec3b367f017f
meto-data <meto-data@users.noreply.github.com>
2026-01-07 09:41:58 +0300

    guncelleme yaptim iste

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,7 +21,6 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
     beforeBody: [
-        Component.TestNotification(),
         Component.ConditionalRender({
             component: Component.Breadcrumbs(),
             condition: (page) => page.fileData.slug !== "index",
@@ -53,7 +52,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-    beforeBody: [Component.TestNotification(), Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+    beforeBody: [/* Component.TestNotification(), */ Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
     left: [
         Component.PageTitle(),
         Component.MobileOnly(Component.Spacer()),
